4ea0d60cd39878500704fd2ac39a62c4f83188bf
Nicholas Orlowsky <nickorlow@nickorlow.com>
2024-03-22 22:57:15 -0500

    update proto

--- server/main.go
+++ server/main.go
@@ -4,11 +4,11 @@ import (
     "log"
     "net/http"
     "fmt"
-    "github.com/gorilla/websocket"
+    "strings"
+    "time"
     "racer/server/pb"
 )
 
-var upgrader = websocket.Upgrader{}
 var todoList []string
 
 func main() {
@@ -17,38 +17,27 @@ func main() {
         Y: 4.2,
         Z: 4.2,
     }
+
     log.Print(vec)
-    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-        // Upgrade upgrades the HTTP server connection to the WebSocket protocol.
-        conn, err := upgrader.Upgrade(w, r, nil)
-        if err != nil {
-            log.Print("upgrade failed: ", err)
-            return
-        }
-        defer conn.Close()
 
-        for {
-            mt, message, err := conn.ReadMessage()
-            if err != nil {
-                log.Println("read failed:", err)
-                break
-            }
-            input := string(message)
-            log.Println(input)
-            output := "No More ?'s\n"
-            message = []byte(output)
-            err = conn.WriteMessage(mt, message)
-
-            if err != nil {
-                log.Println("write failed:", err)
-                break
-            }
-        }
-    })
-
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/ws/", func(w http.ResponseWriter, r *http.Request) {
+        id := strings.TrimPrefix(r.URL.Path, "/ws/")
+        log.Println(id)
+		serveWs(hub, w, r)
+	})
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Welcome to my website!")
     })
 
-    http.ListenAndServe(":8080", nil)
+	server := &http.Server{
+        Addr:              "localhost:8080",
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
