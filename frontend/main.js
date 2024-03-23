e502540b25db1ddf1eee1b15bb6663ec3dfe69a6
k <k@wireless-10-147-31-25.public.utexas.edu>
2024-03-23 04:00:03 -0500

    wip

--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,8 @@
 import * as THREE from 'three';
 
-//const scene = new THREE.Scene();
 var scene = new THREE.Scene();
-//scene.background = new THREE.Color();
-//scene.add(new THREE.AmbientLight(0xffffff, Math.PI));
-//const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const fov = 75; // AKA Field of View
-//const aspect = 1;
 const aspect = window.innerWidth / window.innerHeight;
 const near = 0.1; // the near clipping plane
 const far = 1000; // the far clipping plane
@@ -16,60 +11,30 @@ camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 0;
 // we start pointing in the negative Z direction
-//camera.setRotationFromAxisAngle(new THREE.Vector3(1, 0, 0), 0);
-//camera.rotateY(-Math.PI/2); // initializes us pointing in the positive X direction
+
+const color = 0xFFFFFF;
+const intensity = 100;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(0, 10, 0);
+light.target.position.set(0, 0, -1);
+scene.add(light);
+scene.add(light.target);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//Create a DirectionalLight and turn on shadows for the light
-const light = new THREE.DirectionalLight( 0xffffff, 10 );
-light.position.set( 0, 0, 1 ); //default; light shining from top
-light.castShadow = true; // default false
-scene.add( light );
-
-//Set up shadow properties for the light
-/*
-light.shadow.mapSize.width = 512; // default
-light.shadow.mapSize.height = 512; // default
-light.shadow.camera.near = 0.5; // default
-light.shadow.camera.far = 500; // default
-*/
-
-/*
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-*/
-
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-//import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
-const gloader = new GLTFLoader();
-//const rloader = new DRACOLoader();
-//rloader.setDecoderPath( '/examples/jsm/libs/draco/' );
-//gloader.setDRACOLoader(rloader);
 import { PCDLoader } from 'three/addons/loaders/PCDLoader.js';
 const dloader = new PCDLoader();
-/*
-import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader.js';
-const yloader = new PLYLoader();
-*/
 
-/*
-gloader.load('public/mesh_aligned-transformed.glb', function (nonsense) {
-    console.log(nonsense);
-    scene.add(nonsense.scene);
-}, undefined, function (error) {
-    console.error( error );
-});
-*/
-
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+const gloader = new GLTFLoader();
 
 dloader.load('public/pcd_aligned.pcd', function (points) {
+//gloader.load('public/mesh2.glb', function (points) {
     //console.log(points);
 	//scene.add( gltf.scene );
+    //points = points.scene;
     points.rotateX(-Math.PI/2);
     points.rotateZ(Math.PI/2);
     console.log(points);
@@ -77,7 +42,7 @@ dloader.load('public/pcd_aligned.pcd', function (points) {
     var width = bbox.max.x - bbox.min.x;
     var height = bbox.max.y - bbox.min.y;
     var depth = bbox.max.z - bbox.min.z;
-    points.position.x = width/2;
+    //points.position.x = width/2;
     points.position.y = height/2;
     points.position.z = -depth/2;
     scene.add(points);
@@ -85,121 +50,70 @@ dloader.load('public/pcd_aligned.pcd', function (points) {
 	console.error( error );
 });
 
-
-/*
-yloader.load('public/mesh_aligned.ply', function (p) {
-    console.log(':3');
-}, undefined, function (error) {
-    console.error( error );
-});
-*/
-
 var motioncst = 0.01;
-var navmode = "driving";
 var dirvec = new THREE.Vector3(0, 0, -1);
-    
-if (navmode == "flying") {
+var accel = 0;
+var turndir = 0;
 
-    var handleW = function () {
-        camera.position.z -= motioncst;
-    };
-    var handleA = function () {
-        camera.position.x -= motioncst;
-    };
-    var handleS = function () {
-        camera.position.z += motioncst;
-    };
-    var handleD = function () {
-        camera.position.x += motioncst;
-    };
-    var handleZ = function () {
-        camera.position.y -= motioncst;
-    };
-    var handleX = function () {
-        camera.position.y += motioncst;
-    };
-    var handleJ = function () {
-        camera.rotateZ(-motioncst);
-    };
-    var handleL = function () {
-        camera.rotateZ(motioncst);
-    };
-    var handleI = function () {
-        camera.rotateX(-motioncst);
-    };
-    var handleK = function () {
-        camera.rotateX(motioncst);
-    };
-    var handleN = function () {
-        camera.rotateY(-motioncst);
-    };
-    var handleM = function () {
-        camera.rotateY(motioncst);
-    };
-
-} else if (navmode == "driving") {
-    camera.position.y = 0.04;
-    var handleW = function () {
-        // forwards
-        camera.position.x += (motioncst * dirvec.x);
-        camera.position.y += (motioncst * dirvec.y);
-        camera.position.z += (motioncst * dirvec.z);
-    };
-    var handleA = function () {
+var handleW = function () {
+    // forwards
+    camera.position.x += (motioncst * dirvec.x);
+    camera.position.y += (motioncst * dirvec.y);
+    camera.position.z += (motioncst * dirvec.z);
+};
+var turn = function () {
+    if (turndir != 0) {
         dirvec.applyAxisAngle(new THREE.Vector3(0, 1, 0), motioncst);
         camera.rotateY(motioncst);
         console.log(dirvec);
-    };
-    var handleS = function () {
-        // backwards
-        camera.position.x -= (motioncst * dirvec.x);
-        camera.position.y -= (motioncst * dirvec.y);
-        camera.position.z -= (motioncst * dirvec.z);
-    };
-    var handleD = function () {
-        dirvec.applyAxisAngle(new THREE.Vector3(0, 1, 0), -motioncst);
-        camera.rotateY(-motioncst);
-        console.log(dirvec);
-    };
-    var handleZ = function () {
-        camera.position.y -= motioncst;
-    };
-    var handleX = function () {
-        camera.position.y += motioncst;
-    };
+    }
+};
+var turn_right = function () {
+    dirvec.applyAxisAngle(new THREE.Vector3(0, 1, 0), -motioncst);
+    camera.rotateY(-motioncst);
+    console.log(dirvec);
+};
+var handleS = function () {
+    // backwards
+    camera.position.x -= (motioncst * dirvec.x);
+    camera.position.y -= (motioncst * dirvec.y);
+    camera.position.z -= (motioncst * dirvec.z);
+};
+
+var handleZ = function () {
+    camera.position.y -= motioncst;
+};
+var handleX = function () {
+    camera.position.y += motioncst;
+};
     
-}
-    
-document.addEventListener("keypress", function (e) {
+document.addEventListener("keydown", function (e) {
     e = e || window.event;
 
     if (e.key == 'w') {
-        handleW();
+        accel = 1;
     } else if (e.key == 'a') {
-        handleA();
+        turndir = -1;
     } else if (e.key == 's') {
-        handleS();
+        accel = -1;
     } else if (e.key == 'd') {
-        handleD();
-    } else if (e.key == 'z') {
-        handleZ();
-    } else if (e.key == 'x') {
-        handleX();
-    } else if (e.key == 'j') {
-        handleJ();
-    } else if (e.key == 'l') {
-        handleL();
-    } else if (e.key == 'i') {
-        handleI();
-    } else if (e.key == 'k') {
-        handleK();
-    } else if (e.key == 'n') {
-        handleN();
-    } else if (e.key == 'm') {
-        handleM();
+        turndir = 1;
     }
-
 }, null);
+
+document.addEventListener("keyup", function (e) {
+    e = e || window.event;
+
+    if (e.key == 'w') {
+        accel = 0;
+    } else if (e.key == 'a') {
+        turndir = 0;
+    } else if (e.key == 's') {
+        accel = 0;
+    } else if (e.key == 'd') {
+        turndir = 0;
+    }
+});
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
@@ -233,18 +147,14 @@ const cube5 = new THREE.Mesh( geometry, material5 );
 cube5.position.set(0, 0, -10);
 scene.add(cube5);
 
-/*
-import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
-var fpc = new FirstPersonControls(camera, document);
-
-const stats = THREE.Stats()
-document.body.appendChild(stats.dom)
-*/
-
-function animate() {
+var prev_timestamp = Date.now();
+    
+function animate () {
+    var now = Date.now();
+    var elapsed = now - prev_timestamp;
+    prev_timestamp = now;
+    
 	requestAnimationFrame(animate);
-	//cube.rotation.x += 0.01;
-	//cube.rotation.y += 0.01;
 	renderer.render(scene, camera);
 }
 animate();
